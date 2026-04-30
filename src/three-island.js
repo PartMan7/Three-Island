e87b1cb2d4c4713d5ec5d79f6036ab6b5eb145c6
PartMan <parth.mane@sprinklr.com>
2026-04-30 14:13:25 +0530

    chore: Fix handling for known non-team routes on crob.at

--- src/three-island.js
+++ src/three-island.js
@@ -118,8 +118,7 @@ function fetchPaste(url) {
         break;
       }
       case !!(pattern = url.match(/crob\.at\/([a-zA-Z0-9]+)/)): {
-        if (pattern[1] === 'constructor')
-          return reject(new Error('Well someone tried to screw me up'));
+        if (['constructor', 'api'].includes(pattern[1])) return;
         const cacheKey = `crob.at:${pattern[1]}`;
         if (generatedPasteHTML[cacheKey]) return resolve(cacheKey);
         const jsonLink = `https://crob.at/api/team/${pattern[1]}`;
@@ -244,15 +243,16 @@ function loadPaste(url) {
       default:
         return reject(new Error('Invalid paste'));
     }
-    if (paste[1] === 'constructor')
-      return reject(new Error('Well someone tried to screw me up'));
+    if (['constructor', 'api'].includes(paste[2])) return;
     const cacheKey = paste[1] + ':' + paste[2];
     if (generatedPasteHTML[cacheKey])
       return resolve(generatedPasteHTML[cacheKey]);
     else
       return fetchPaste(url).then((data) => resolve(generatedPasteHTML[data]));
   })
-    .then((data) => addTeam(data))
+    .then((data) => {
+      if (data) addTeam(data);
+    })
     .catch(error);
 }
 
@@ -285,6 +285,7 @@ function runCheck(ftd, isPM) {
     const link = ftd.href;
     fetchPaste(link)
       .then((ref) => {
+		if (!ref) return;
         const data = generatedPasteHTML[ref];
         const tooltip = document.createElement('span');
         if (isPM) tooltip.classList.add('threeisland-pm');
@@ -318,7 +319,7 @@ function runCheck(ftd, isPM) {
           }
         });
         button.appendChild(document.createTextNode('Import'));
-		button.style.margin = '4px';
+        button.style.margin = '4px';
         tooltip.appendChild(button);
         ftd.classList.add('threeisland-link');
         if (isPM) ftd.classList.add('threeisland-pm');
@@ -434,14 +435,18 @@ function watchRoom(node, spc) {
   return observerW;
 }
 
-Object.entries(IS_REWRITE_CLIENT ? PS.rooms : app.rooms).forEach(([room, data]) => {
-  // Load all rooms on connecting
-  const val = validRoom(room);
-  if (!val) return;
-  const node = IS_REWRITE_CLIENT ? document.querySelector(`#room-${room}`) : data.el;
-  const observerR = watchRoom(node, val);
-  ROOM_OBSERVERS[room] = { node, observerR };
-});
+Object.entries(IS_REWRITE_CLIENT ? PS.rooms : app.rooms).forEach(
+  ([room, data]) => {
+    // Load all rooms on connecting
+    const val = validRoom(room);
+    if (!val) return;
+    const node = IS_REWRITE_CLIENT
+      ? document.querySelector(`#room-${room}`)
+      : data.el;
+    const observerR = watchRoom(node, val);
+    ROOM_OBSERVERS[room] = { node, observerR };
+  },
+);
 
 const observer = new MutationObserver((mutations) => {
   // Keep an eye out for rooms we're joining / leaving
@@ -476,7 +481,9 @@ const observerPM = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     for (const node of mutation.addedNodes) {
       if (node.nodeType !== 1) continue;
-      const user = IS_REWRITE_CLIENT ? node.getAttribute('data-roomid').replace('dm-', '') : node.getAttribute('data-userid');
+      const user = IS_REWRITE_CLIENT
+        ? node.getAttribute('data-roomid').replace('dm-', '')
+        : node.getAttribute('data-userid');
       if (!user) continue;
       const observerB = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
@@ -500,7 +507,12 @@ const observerPM = new MutationObserver((mutations) => {
     }
   });
 });
-observerPM.observe(document.querySelector(IS_REWRITE_CLIENT ? '.mainmenu-mini-windows' : '.pmbox'), { childList: true });
+observerPM.observe(
+  document.querySelector(
+    IS_REWRITE_CLIENT ? '.mainmenu-mini-windows' : '.pmbox',
+  ),
+  { childList: true },
+);
 
 function addCSS(css) {
   // And finally we make it look pretty
